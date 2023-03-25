4091f0783654cafa2ac2638bf2d50df0305631f9
Dessmondd <97458634+Dessmondd@users.noreply.github.com>
2023-03-25 22:11:22 +0100

    Added File Uploading.
    This version gives the user more detailed information about the virus itself, instead of only say if the URL itself is malicous and that's it.
    A new feature was added, in this case is a early stage of file uploading. This feature will work much better once i add the entire tool a GUI for a easier use for non-experienced users.
    In the future, those URL's and files will get check by more antivirus and will be filtered with more options for a safer usage.

--- WebURLChecker.py
+++ WebURLChecker.py
@@ -1,24 +1,60 @@
 import os
 import requests
 
+VIRUSTOTAL_API_KEY = "YOUR_API_KEY"
 
-url_tocheck = input("Please, provide the website or link to check: ")
+def file_checker(file_path):
+    with open(file_path, "rb") as f:
+        file_data = f.read()
+
+    url = "https://www.virustotal.com/api/v3/files"
+    headers = {
+        "x-apikey": VIRUSTOTAL_API_KEY
+    }
+    files = {
+        "file": (os.path.basename(file_path), file_data)
+    }
+    response = requests.post(url, headers=headers, files=files)
+
+    if response.status_code == 200:
+        json_response = response.json()
+        data = json_response["data"]
+        attributes = data["attributes"]
+        results = attributes["last_analysis_results"]
+        positives = sum(1 for result in results.values() if result["category"] == "malicious")
+        if positives > 0:
+            return True
+        else:
+            return False
+    else:
+        raise Exception(f"File scan failed with status code {response.status_code}")
+
 
 def url_checker(url_tocheck):
-    api_key="APIKEY"
-    params = {'apikey': api_key, 'resource': url_tocheck}
-    headers = {
-            "Accept-Encoding": "gzip, deflate",
-            "User-Agent": "gzip, My Python requests library"
+    url = "https://www.virustotal.com/api/v3/urls"
+    params = {
+        "url": url_tocheck
     }
-    response = requests.get(url_tocheck, params, headers=headers)
-    response_json = response.json()
-    if response_json['response_code'] == 0:
-        print("Link is not malicious, you are safe!")
-        return False
-    if response_json['positives'] == 1:
-        # URL it's malicious based on at least ONE 
-        return True
-    else: 
-        print("It's not malicious.")
+    headers = {
+        "x-apikey": VIRUSTOTAL_API_KEY
+    }
+    response = requests.get(url, params=params, headers=headers)
 
+    if response.status_code == 200:
+        json_response = response.json()
+        data = json_response["data"]
+        attributes = data["attributes"]
+        categories = attributes["categories"]
+        if "malicious" in categories:
+            return True
+        else:
+            return False
+    else:
+        raise Exception(f"URL scan failed with status code {response.status_code}")
+
+
+def scanner(file_or_url):
+    if os.path.isfile(file_or_url):
+        return file_checker(file_or_url)
+    else:
+        return url_checker(file_or_url)
